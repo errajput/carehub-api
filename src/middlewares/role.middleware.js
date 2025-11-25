a8f7cd1f085c50da0d0467aef3380cdf9c6a1d81
disha rajput <dishacsevira@gmail.com>
2025-11-25 11:24:55 +0530

    Create middleware

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,12 @@
+export default function roleMiddleware(required) {
+  return function (req, res, next) {
+    if (!req.user) return res.status(401).json({ message: "No user" });
+
+    const roles = Array.isArray(required) ? required : [required];
+
+    if (!roles.includes(req.user.role))
+      return res.status(403).json({ message: "Forbidden" });
+
+    next();
+  };
+}
